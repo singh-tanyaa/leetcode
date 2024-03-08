1ea0909944c0db68d73220f245dba648fd31a3b4
TANYA <96328928+singh-tanyaa@users.noreply.github.com>
2024-03-09 01:27:56 +0530

    Time: 4 ms (89.99%), Space: 8.4 MB (67.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
-        
+        if(s.length() != t.length()) {
+            return false;
+        }
+        vector<int>arr(26,0);
+         for(int i = 0; i < s.length(); i++) {
+            arr[s[i] - 'a']++;
+            arr[t[i] - 'a']--;
+        }
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] > 0) {
+                return false;
+            }
+        }
+        return true;
     }
 };
