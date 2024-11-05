bdef35821921e1ffe44f4924b897a5516336e071
TANYA <96328928+singh-tanyaa@users.noreply.github.com>
2024-11-05 22:29:32 +0530

    Time: 0 ms (100.00%), Space: 19.4 MB (86.68%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+       int n=(int) nums.size();
+       int a=0;
+       for(int i=0;i<n;i++){
+           a=a^nums[i];
+       }
+       return a;
+    }
+};
